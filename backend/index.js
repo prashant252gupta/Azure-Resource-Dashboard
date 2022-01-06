7f87f0e52f376f5db70adc987240966f7e2c745d
prashant252gupta <pguptaprashant252@gmail.com>
2022-01-06 20:17:17 +0530

    deploy changes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const resourceRoute = require('./routes/resources')
 const connectDB = require('./config/DB_CONFIG');
 const cors = require('cors')
+const path = require('path');
 //Connecting to Database
 connectDB();
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.use('/azure', resourceRoute)
 
